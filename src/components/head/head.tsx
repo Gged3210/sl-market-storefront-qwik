25f4e2be964d8a417d6a36e485f3bcd9057916fc
Edwin @ WSL2 Ubuntu <asib32102@gmail.com>
2024-06-06 23:23:00 +0800

    added Partytown with Gtag and Ms Clarity

--- src/components/head/head.tsx
+++ src/components/head/head.tsx
@@ -4,6 +4,7 @@ import { Insights } from '@builder.io/qwik-labs';
 import { DEFAULT_METADATA_TITLE } from '~/constants';
 import { ENV_VARIABLES } from '~/env';
 import { generateDocumentHead } from '~/utils';
+import { QwikPartytown } from '../partytown/partytown';
 
 export const Head = component$(() => {
 	const documentHead = useDocumentHead();
@@ -14,6 +15,35 @@ export const Head = component$(() => {
 	return (
 		<head>
 			<meta charSet="utf-8" />
+			<QwikPartytown forward={['gtag', 'dataLayer.push']} />
+			{/* Google Tag Manager */}
+			<script
+				async
+				type="text/partytown"
+				src="https://www.googletagmanager.com/gtag/js?id=G-8KSG59E7TC"
+			/>
+			<script
+				type="text/partytown"
+				dangerouslySetInnerHTML={`
+					window.dataLayer = window.dataLayer || [];
+					window.gtag = function() {
+					dataLayer.push(arguments);
+					}
+					gtag('js', new Date());
+					gtag('config', 'G-8KSG59E7TC');
+				`}
+			/>
+			{/* MS Clarity */}
+			<script
+				type="text/partytown"
+				dangerouslySetInnerHTML={`
+					(function(c,l,a,r,i,t,y){
+						c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+						t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+						y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+					})(window, document, "clarity", "script", "mnlgw1uilh");
+				`}
+			/>
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
 			<meta name="theme-color" content="#1D4ED8" />
 			<title>{head.title || DEFAULT_METADATA_TITLE}</title>
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { qwikInsights } from '@builder.io/qwik-labs/vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { partytownVite } from '@builder.io/partytown/utils';
+import { join } from 'path';
 
 export default defineConfig(async (config) => {
 	return {
@@ -36,6 +38,7 @@ export default defineConfig(async (config) => {
 				},
 			}),
 			tsconfigPaths(),
+			partytownVite({ dest: join(__dirname, 'dist', '~partytown') }),
 		],
 		preview: {
 			headers: {
